23a705d7a852ef059be56fd380001cd241705a00
thomascaldana <thomascaldana@gmail.com>
2023-06-10 11:14:20 -0300

    adding new images that client asked

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -6,6 +6,10 @@ import trabalho3 from "../../assets/trabalho3.jpg";
 import trabalho4 from "../../assets/trabalho4.jpg";
 import trabalho5 from "../../assets/trabalho5.jpg";
 import trabalho6 from "../../assets/trabalho6.jpg";
+import trabalho7 from "../../assets/trabalho7.jpg";
+import trabalho8 from "../../assets/trabalho8.jpg";
+import trabalho9 from "../../assets/trabalho9.jpg";
+import trabalho10 from "../../assets/trabalho10.jpg";
 
 function Jobs() {
   return (
@@ -18,6 +22,10 @@ function Jobs() {
         <img src={trabalho4} alt="movel reformado" />
         <img src={trabalho5} alt="movel reformado" />
         <img src={trabalho6} alt="movel reformado" />
+        <img src={trabalho7} alt="movel reformado" />
+        <img src={trabalho8} alt="movel reformado" />
+        <img src={trabalho9} alt="movel reformado" />
+        <img src={trabalho10} alt="movel reformado" />
       </Images>
     </Container>
   );
