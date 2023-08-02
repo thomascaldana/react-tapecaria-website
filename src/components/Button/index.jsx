af5b0cd9b6da7c248170b32612404c1eff58d42a
thomascaldana <thomascaldana@gmail.com>
2023-08-02 12:24:19 -0300

    changing the whatsApp link

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,10 @@
 import { Link, BlueButton } from "./styles";
 import WhatsAppLogo from "../../assets/logo-whatsapp.png";
 
-function Button() {
+function Button () {
   return (
     <Link
-      href="https://wa.me/5515997205991?text=Estava%20no%20site%20e%20gostaria%20de%20fazer%20um%20or%C3%A7amento..."
+      href="https://wa.me/5519996018238?text=Estava%20no%20site%20e%20gostaria%20de%20fazer%20um%20or%C3%A7amento..."
       target="_blank"
     >
       <BlueButton>
