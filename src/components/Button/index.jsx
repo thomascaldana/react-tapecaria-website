9a3a6ff35d075b2b34f29330a14c7499d07c6cc0
thomascaldana <thomascaldana@gmail.com>
2023-08-18 09:10:02 -0300

    updating whatsapp link

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,7 +5,7 @@ import WhatsAppLogo from "../../assets/logo-whatsapp.png";
 function Button () {
   return (
     <Link
-      href="https://wa.me/5519996018238?text=Estava%20no%20site%20e%20gostaria%20de%20fazer%20um%20or%C3%A7amento..."
+      href="https://wa.me/5515997205991?text=Estava%20no%20site%20e%20gostaria%20de%20fazer%20um%20or%C3%A7amento..."
       target="_blank"
     >
       <BlueButton>
