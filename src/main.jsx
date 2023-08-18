fa68598c7a3c775a00bd58f1130dffb7a5e4a217
thomascaldana <thomascaldana@gmail.com>
2023-08-18 14:21:03 -0300

    adding web analytics importation vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../src/components/Home";
 import GlobalStyle from "./styles/globalStyles";
+import { inject } from '@vercel/analytics';
+
+inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
