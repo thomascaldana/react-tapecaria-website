ce1bc2b44983d0ae21a6dfa460d5605eb2e67d97
thomascaldana <thomascaldana@gmail.com>
2023-08-20 14:33:47 -0300

    removing analytcs from vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../src/components/Home";
 import GlobalStyle from "./styles/globalStyles";
-import { inject } from '@vercel/analytics';
-
-inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
